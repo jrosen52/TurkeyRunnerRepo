290954ad31426ac5483e5b775ae4239acfaee80d
Joshua Rosenthal <jrosen52@binghamton.edu>
2019-12-06 17:22:43 -0500

    sprite update

--- app/src/main/java/com/example/turkeyrunner/Sprite.java
+++ app/src/main/java/com/example/turkeyrunner/Sprite.java
@@ -5,6 +5,7 @@ import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Path;
 import android.graphics.Rect;
 
 public class Sprite
@@ -49,6 +50,22 @@ public class Sprite
         this.x = hitbox.left;
         this.y = hitbox.top;
 
+        this.vx = 0;
+        this.vy = 0;
+        this.ax = 0;
+        this.ay = 0;
+
+        noAliasPaint.setAntiAlias(false);
+        noAliasPaint.setFilterBitmap(false);
+        noAliasPaint.setDither(false);
+        noAliasPaint.setColor(Color.GREEN);
+
+        borderPaint.setStrokeWidth(10);
+        borderPaint.setStyle(Paint.Style.STROKE);
+
+        vectorPaint.setStyle(Paint.Style.STROKE);
+        vectorPaint.setStrokeWidth(8);
+        vectorPaint.setColor(Color.GREEN);
     }
 
     public void draw(Canvas canvas, long elevation) {
@@ -63,18 +80,79 @@ public class Sprite
         //drawVecs(canvas, elevation, 15);
     }
 
+    public void update(long elapsed) {
+        // Update velocities
+        vx += ax;
+        vy += ay;
+
+        // Update positions
+        if(this.affectedByGrav) vy += GRAV;
+        setX(this.getX() + vx);
+        setY(this.getY() + vy);
+    }
+
+
     public void drawHitbox(Canvas canvas, long elevation, int color) {
         borderPaint.setColor(color);
         this.setY(this.getY());// + elevation);
         canvas.drawRect(hitbox, borderPaint);
     }
 
-    public double getX() {return this.x;}
+    public void drawVecs(Canvas canvas, long elevation, int scalar) {
+        // Draw velocities
+        Path path = new Path();
+        path.moveTo(this.getHitbox().centerX(), this.getHitbox().centerY());
+        path.lineTo(this.getHitbox().centerX() + (int) vx * scalar,
+                this.getHitbox().centerY() + (int) vy * scalar);
+        path.close();
+        canvas.drawPath(path, vectorPaint);
+    }
 
-    public double getY() {return this.y;}
+    public Rect getHitbox() {
+        return hitbox;
+    }
+
+    public int getHeight() {
+        return this.height;
+    }
+
+    public int getWidth() {
+        return this.width;
+    }
+
+    public double getX() {
+        return this.x;
+    }
+
+    public double getY() {
+        return this.y;
+    }
+
+    public double getRight() {
+        return this.x + this.getWidth();
+    }
+
+    public double getBottom() {
+        return this.y + this.getHeight();
+    }
+
+    public void setX(double x) {
+        this.x = x;
+        this.getHitbox().set(
+                (int) x,
+                (int) y,
+                (int) x + this.getWidth(),
+                (int) y + this.getHeight()
+        );
+    }
 
     public void setY(double y) {
         this.y = y;
-        //this.hitbox.offsetTo((int) this.x, (int) this.y);
+        this.getHitbox().set(
+                (int) x,
+                (int) y,
+                (int) x + this.getWidth(),
+                (int) y + this.getHeight()
+        );
     }
 }
